e895ee3c9a9611431388e49ccc09beee9f4f64e6
Rainer Ramos <rainermr89@hotmail.com>
2021-08-24 16:27:23 -0300

    Lógica da aplicação

--- app/src/main/java/com/rainerramos/myapplication/MainActivity.kt
+++ app/src/main/java/com/rainerramos/myapplication/MainActivity.kt
@@ -16,12 +16,29 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View) {
         val id = view.id
-        if (id == R.id.buttonCalculate){
+        if (id == R.id.buttonCalculate) {
             calculate()
         }
     }
 
     private fun calculate() {
+        if (validationOk()) {
+            val distance = editDistance.text.toString().toFloat()
+            val price = editPrice.text.toString().toFloat()
+            val autonomy = editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
+            textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+
+        }
+    }
+
+    private fun validationOk(): Boolean {
+        return (editDistance.text.toString() != ""
+                && editPrice.text.toString() != ""
+                && editAutonomy.text.toString() != "")
+
 
     }
+
 }
